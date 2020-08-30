3f93ac6b27030cd6b5f049ba4ebf921e38b022f5
Satyam Singhania <45529137+Shadow-Cyng@users.noreply.github.com>
2020-08-30 19:07:16 +0530

    Updates

---
+++ precache-manifest.82f9249c38562d32b2fc284fd4f1d531.js
@@ -0,0 +1,214 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d764dff1210f4a8a6c84209e83bf014",
+    "url": "/digi-portfolio/index.html"
+  },
+  {
+    "revision": "bf846c2a5993777835b2",
+    "url": "/digi-portfolio/static/css/2.ac1ab58b.chunk.css"
+  },
+  {
+    "revision": "3811d94f280f68e4b87d",
+    "url": "/digi-portfolio/static/css/main.ef97bc28.chunk.css"
+  },
+  {
+    "revision": "bf846c2a5993777835b2",
+    "url": "/digi-portfolio/static/js/2.d4fce170.chunk.js"
+  },
+  {
+    "revision": "cf1b129b5c272ab5c924fa9150c8b22d",
+    "url": "/digi-portfolio/static/js/2.d4fce170.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3811d94f280f68e4b87d",
+    "url": "/digi-portfolio/static/js/main.619a6cd9.chunk.js"
+  },
+  {
+    "revision": "aed40a4748589ccd0f78",
+    "url": "/digi-portfolio/static/js/runtime-main.df4e3c87.js"
+  },
+  {
+    "revision": "bc5b7ac1c7db197d9b9f4d14bc636bde",
+    "url": "/digi-portfolio/static/media/1.bc5b7ac1.webp"
+  },
+  {
+    "revision": "f03fe9305afb20c5a07fdca17c85e87d",
+    "url": "/digi-portfolio/static/media/10.f03fe930.webp"
+  },
+  {
+    "revision": "b6231449bf4b73d23aad9d5ddc955cd0",
+    "url": "/digi-portfolio/static/media/11.b6231449.webp"
+  },
+  {
+    "revision": "63232f64761efb39af01c0b600361254",
+    "url": "/digi-portfolio/static/media/12.63232f64.webp"
+  },
+  {
+    "revision": "f5660832c330c5fb561cef65b1cd94ad",
+    "url": "/digi-portfolio/static/media/13.f5660832.webp"
+  },
+  {
+    "revision": "0c81b41ef736522f3cc7d48b254e8aaf",
+    "url": "/digi-portfolio/static/media/14.0c81b41e.webp"
+  },
+  {
+    "revision": "50b73ee52848f8cee6a914e143fbb091",
+    "url": "/digi-portfolio/static/media/15.50b73ee5.webp"
+  },
+  {
+    "revision": "3f9d59d8ed08a389d2cefc59ea973c05",
+    "url": "/digi-portfolio/static/media/16.3f9d59d8.webp"
+  },
+  {
+    "revision": "c421c598643e7cc1b50c61f2d53bcf6f",
+    "url": "/digi-portfolio/static/media/17.c421c598.webp"
+  },
+  {
+    "revision": "86801bbc1f94b4c6aa1b71ad2d57f4d9",
+    "url": "/digi-portfolio/static/media/2.86801bbc.webp"
+  },
+  {
+    "revision": "b6e280bfc12b0151a5dcd7afe4503921",
+    "url": "/digi-portfolio/static/media/3.b6e280bf.webp"
+  },
+  {
+    "revision": "9014d08cd9013677d9dabadf24a447a5",
+    "url": "/digi-portfolio/static/media/4.9014d08c.webp"
+  },
+  {
+    "revision": "bbb3370e5e7f3ce8430325c4dc108eef",
+    "url": "/digi-portfolio/static/media/5.bbb3370e.webp"
+  },
+  {
+    "revision": "f60bbbfebae3a92d4822454a9d571f86",
+    "url": "/digi-portfolio/static/media/6.f60bbbfe.webp"
+  },
+  {
+    "revision": "6e38efd1145fd88b464a763386620cc6",
+    "url": "/digi-portfolio/static/media/7.6e38efd1.webp"
+  },
+  {
+    "revision": "50b73ee52848f8cee6a914e143fbb091",
+    "url": "/digi-portfolio/static/media/8.50b73ee5.webp"
+  },
+  {
+    "revision": "178df9dffb228e66ce87a42d84cf3b15",
+    "url": "/digi-portfolio/static/media/9.178df9df.webp"
+  },
+  {
+    "revision": "1fc9cc1428403ae7fe43f0af487cc9a9",
+    "url": "/digi-portfolio/static/media/Chat-Box.1fc9cc14.webp"
+  },
+  {
+    "revision": "168a2808a97b064b374235e3dafbe046",
+    "url": "/digi-portfolio/static/media/Face-Detection.168a2808.webp"
+  },
+  {
+    "revision": "673de975f3004196b90cc69b22f63c66",
+    "url": "/digi-portfolio/static/media/Insta-clone.673de975.webp"
+  },
+  {
+    "revision": "258564414294b36ca918c8dbdd6eca7d",
+    "url": "/digi-portfolio/static/media/Scream.25856441.webp"
+  },
+  {
+    "revision": "6e42c37f49d2c87493cdef31abd07355",
+    "url": "/digi-portfolio/static/media/adobe-photoshop.6e42c37f.svg"
+  },
+  {
+    "revision": "e95d05630894e5403e569fdaa0a2b61a",
+    "url": "/digi-portfolio/static/media/background.e95d0563.webp"
+  },
+  {
+    "revision": "ffded2381d25dd906406d6e07f91dea5",
+    "url": "/digi-portfolio/static/media/bootstrap-4.ffded238.svg"
+  },
+  {
+    "revision": "3707da3c6a394c7ee2523273f8d98a25",
+    "url": "/digi-portfolio/static/media/css3.3707da3c.svg"
+  },
+  {
+    "revision": "c6bab64b183f96aa47077fd3db37afd0",
+    "url": "/digi-portfolio/static/media/express.c6bab64b.svg"
+  },
+  {
+    "revision": "1c415532b1999396ccad8b51a601e36e",
+    "url": "/digi-portfolio/static/media/firebase.1c415532.svg"
+  },
+  {
+    "revision": "1190a19ce3cc940741a60137a1e074cb",
+    "url": "/digi-portfolio/static/media/git-icon.1190a19c.svg"
+  },
+  {
+    "revision": "b38ff61aa327fbdc24f7b1dad7d04d1a",
+    "url": "/digi-portfolio/static/media/heroku.b38ff61a.svg"
+  },
+  {
+    "revision": "676e8188f003ed5d4882a0d0f378dad3",
+    "url": "/digi-portfolio/static/media/html-5.676e8188.svg"
+  },
+  {
+    "revision": "13a096531e50277a3fb17460c39db221",
+    "url": "/digi-portfolio/static/media/java.13a09653.svg"
+  },
+  {
+    "revision": "2d2e31cc63a96c7e6044f52d30fd21b9",
+    "url": "/digi-portfolio/static/media/javascript.2d2e31cc.svg"
+  },
+  {
+    "revision": "82291019f34ab54a677c456a84127cc3",
+    "url": "/digi-portfolio/static/media/material-ui-1.82291019.svg"
+  },
+  {
+    "revision": "b30ceef7f7e81c7dc160af90229e9775",
+    "url": "/digi-portfolio/static/media/mongodb.b30ceef7.svg"
+  },
+  {
+    "revision": "54d7bddcd3019ec3c3b23db00160ccef",
+    "url": "/digi-portfolio/static/media/mssql.54d7bddc.svg"
+  },
+  {
+    "revision": "a123152868245dee71c441124a98ac3e",
+    "url": "/digi-portfolio/static/media/nodejs.a1231528.svg"
+  },
+  {
+    "revision": "d7c985e7cca077dc2d77f89a66573e35",
+    "url": "/digi-portfolio/static/media/postgresql.d7c985e7.svg"
+  },
+  {
+    "revision": "8763c5c67d478cf8a019b63b4d456fa2",
+    "url": "/digi-portfolio/static/media/profile.8763c5c6.webp"
+  },
+  {
+    "revision": "83fe61b05935f090b671b08aeb77b63d",
+    "url": "/digi-portfolio/static/media/react-bootstrap.83fe61b0.svg"
+  },
+  {
+    "revision": "024196716802e272adb3cbefb81fd557",
+    "url": "/digi-portfolio/static/media/react-router.02419671.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/digi-portfolio/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "aa0a337603611296524a666b62a9ff23",
+    "url": "/digi-portfolio/static/media/redux.aa0a3376.svg"
+  },
+  {
+    "revision": "b446ca5a397c1ef6fb94cd02c4e94335",
+    "url": "/digi-portfolio/static/media/sass-1.b446ca5a.svg"
+  },
+  {
+    "revision": "8bce7aef886c3872c218e0e6ef8bdccb",
+    "url": "/digi-portfolio/static/media/slide1.8bce7aef.webp"
+  },
+  {
+    "revision": "7256f309520276be70acd97720610bc6",
+    "url": "/digi-portfolio/static/media/slide2.7256f309.webp"
+  },
+  {
+    "revision": "8e5b87b60151049a8df9ec0bd30978f0",
+    "url": "/digi-portfolio/static/media/slide3.8e5b87b6.webp"
+  }
+]);
