1e73333771918f1dd46a756d13ccfdea61813ddb
satyam Singhania <satyam.singhania123@gmail.com>
2021-03-14 19:43:28 +0530

    Yo

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -74,7 +74,7 @@ const About = () => {
 									</Button>
 								</a>
 								<a
-									href='https://drive.google.com/file/d/1VgtKTGHMpJf-o8GDC6eHK2WQxq-JPszK/view?usp=sharing'
+									href='https://drive.google.com/file/d/1vu8xYaMT_mgha3fGTcENNjmyMTAZ9Rzp/view?usp=sharing'
 									target='_blank'
 									rel='noopener noreferrer'
 								>
